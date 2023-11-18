0cac950ca335e4ac4237e04ccf2d3dfb5e84a586
Mouhcine Essafi <messafi1337@gmail.com>
2023-11-18 14:38:39 +0100

    Update supprimer.php

--- supprimer.php
+++ supprimer.php
@@ -1,8 +1,15 @@
 <?php
-
+/**
+ * Include Connection File
+ */
 include('connection.php');
+/**
+ * Get ID
+ */
 $id = $_GET["id"];
-
+/**
+ * delete a annonce with the given 'id' from the table
+ */
 $sql = "DELETE FROM $table_name WHERE id = $id";
 
 $res = $conn->query($sql);
