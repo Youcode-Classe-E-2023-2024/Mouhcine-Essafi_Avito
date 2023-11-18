8d6a53a2e1ff94ee6915790c426a0f4dfd7148ff
Mouhcine Essafi <messafi1337@gmail.com>
2023-11-18 13:38:52 +0100

    supprimer l'annonce selectionner

--- supprimer.php
+++ supprimer.php
@@ -1,1 +1,19 @@
+<?php
 
+include('connection.php');
+$id = $_GET["id"];
+
+$sql = "DELETE FROM $table_name WHERE id = $id";
+
+$res = $conn->query($sql);
+
+if ($res) {
+    header("Location: avito.php");
+    exit();
+} else {
+    echo "Erreur lors de l'exécution de la requête !";
+}
+
+$conn->close();
+
+?>
